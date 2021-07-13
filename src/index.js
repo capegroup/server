280a9109caceb211fe83dec02d2eabbf8565a989
friend <friend@yourfriend.lv>
2021-07-13 14:35:15 +0300

    Add a endpoint for viewing all cosmetics

--- src/index.js
+++ src/index.js
@@ -24,6 +24,10 @@ if(!fs.existsSync("./capes")) {
 }
 
 
+app.get("/cosmetics/view", (req, res) => {
+	res.json(fs.readdirSync("src/cosmetics"))
+})
+
 app.get("/users/:user.cfg", async (req, res) => {
 	let cosmetics = await db.get(req.params.user);
 	
