c65212e23bde386798806a434ba760b652bc48bb
friend <friend@yourfriend.lv>
2021-07-13 14:32:26 +0300

    cosmetics :D

--- src/index.js
+++ src/index.js
@@ -2,16 +2,49 @@ import logSymbols from "log-symbols"
 import express from "express"
 import path from "path"
 import fs from "fs"
+import Josh from "@joshdb/core";
+import provider from "@joshdb/sqlite";
 
 console.log(logSymbols.info, "Starting..")
 
 const app = express()
+
+const db = new Josh({
+	name: 'gm_capegroup',
+	provider
+});
+
 app.use(express.static(path.resolve("src/website")))
+app.use("/cosmetics", express.static(path.resolve("src/cosmetics")))
 
 if(!fs.existsSync("./capes")) {
 	console.log(logSymbols.warning, "capes folder was not created. creating.")
+
 	fs.mkdirSync("capes")
 }
+
+
+app.get("/users/:user.cfg", async (req, res) => {
+	let cosmetics = await db.get(req.params.user);
+	
+	if(!cosmetics) cosmetics = [];
+
+	const final = {
+		items: []
+	}
+
+	cosmetics.forEach(cosmetic => {
+		final.items.push({
+			type: cosmetic,
+			model: `cosmetics/${cosmetic}/model.cfg`,
+			texture: `cosmetics/${cosmetic}/texture.png`,
+			active: true
+		})
+	})
+
+	res.set('Content-Type', 'application/octet-stream');
+	res.send(JSON.stringify(final, null, 2))
+})
 
 app.get('/capes/:username.png', (req, res) => {
 	const username = req.params.username;
